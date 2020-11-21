218b6b073f580a5fa68bbabb7d5b10b2ffa2ecd3
AkoniHakeem <akozymayor@gmail.com>
2020-11-21 09:55:06 +0100

    renamed words counter to character counter

--- WordsCounter/charactersCounter.js
+++
@@ -1,7 +0,0 @@
-window.onload = () => {
-    document.getElementById("wordsArea").addEventListener("input", (e) => {
-        const characters = e.target.value;
-        const count = characters.length;
-        document.getElementById("countsDisplay").innerText = count;
-    })
-}
